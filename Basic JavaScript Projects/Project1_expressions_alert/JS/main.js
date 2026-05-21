3a956597dcb42705c8a3ac2ca4b1c25326b83990
TallyJack <lynchj@infinity-software.com>
2026-05-21 16:59:37 -0400

    Completed Step 59

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,9 +1,12 @@
+/*VAR Definitions*/
 var E = "<br>"
 var A = "Testing...Testing...Anyone there?", B = "Welcome to Jack\'s Experiment with using text in JavaScript! <br> \"It's the best language there is!\" - Some people";
 var claim = "The best movie of all time is ", subject = "The Cat in Hat (2003).";
 
-
+/*RUN*/
 window.alert(A);
 document.write(B+E+E);
-document.write(claim + subject);
+document.write(claim + subject + E);
 
+document.write(100-1);
+document.write("% of people disagree with me and " + (50 + 50) + "% of those people are incorrect.");
