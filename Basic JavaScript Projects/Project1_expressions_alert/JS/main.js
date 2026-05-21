a9c7e68cd57778b6e5fad771d3bff089f4090a9f
TallyJack <lynchj@infinity-software.com>
2026-05-21 17:09:12 -0400

    Completed Step 64

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,12 +1,12 @@
-/*VAR Definitions*/
+//These are the definitions of my VARs
 var E = "<br>"
 var A = "Testing...Testing...Anyone there?", B = "Welcome to Jack\'s Experiment with using text in JavaScript! <br> \"It's the best language there is!\" - Some people";
 var claim = "The best movie of all time is ", subject = "The Cat in Hat (2003).";
 
-/*RUN*/
-window.alert(A);
-document.write(B+E+E);
-document.write(claim + subject + E);
+//This is the script where my statements are executed
+window.alert(A); //This is where the window alert displays the greeting
+document.write(B+E+E); //This is where the user is welcomed on the page itelf
+document.write(claim + subject + E); //This is where an outragoeous claim is made
 
-document.write(100-1);
+document.write(100-1); //This is where I begin to double down on that claim. I used two distinct statements as an experiment with syntax
 document.write("% of people disagree with me and " + (50 + 50) + "% of those people are incorrect.");
