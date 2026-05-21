dd38cc8bab686a91036e61f7348960098c17f8bb
TallyJack <lynchj@infinity-software.com>
2026-05-21 16:25:55 -0400

    Completed Step 57

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,8 +1,6 @@
 var E = "<br>"
-var A = "Testing...Testing...Anyone there?";
-var B = "Welcome to Jack\'s Experiment with using text in JavaScript! <br> \"It's the best language there is!\" - Some people";
-var claim = "The best movie of all time is "
-var subject = "The Cat in Hat (2003)."
+var A = "Testing...Testing...Anyone there?", B = "Welcome to Jack\'s Experiment with using text in JavaScript! <br> \"It's the best language there is!\" - Some people";
+var claim = "The best movie of all time is ", subject = "The Cat in Hat (2003).";
 
 
 window.alert(A);
