2e4060e6ad8aab3381cd27a87e0f078ef6028d61
TallyJack <lynchj@infinity-software.com>
2026-05-22 16:36:58 -0400

    Completed Step 65

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -3,10 +3,15 @@ var E = "<br>"
 var A = "Testing...Testing...Anyone there?", B = "Welcome to Jack\'s Experiment with using text in JavaScript! <br> \"It's the best language there is!\" - Some people";
 var claim = "The best movie of all time is ", subject = "The Cat in Hat (2003).";
 
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
 //This is the script where my statements are executed
 window.alert(A); //This is where the window alert displays the greeting
 document.write(B+E+E); //This is where the user is welcomed on the page itelf
 document.write(claim + subject + E); //This is where an outragoeous claim is made
 
 document.write(100-1); //This is where I begin to double down on that claim. I used two distinct statements as an experiment with syntax
-document.write("% of people disagree with me and " + (50 + 50) + "% of those people are incorrect.");
+document.write("% of people disagree with me and " + (50 + 50) + "% of those people are incorrect."+E+E);
+
+document.write(Sent1+Sent2); //This is the assignment required on step 65
