8e92c25a75fe4716fa268e9eed751f210b0cc706
TallyJack <lynchj@infinity-software.com>
2026-05-26 16:49:04 -0400

    Completed Step 80

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,13 @@
+//These are the definitions of my VARs
+var E = "<br>";
+var A = "Website Reset";
+
+//This is where my functions are defined
+
+function crunchNums() { //This function returns the solution to the math question as a string
+    var V1 = 3 + 3;
+    document.getElementById("Math").innerHTML = "3 + 3 is " + V1;
+}
+
+//This is the script where my statements are executed
+window.alert(A); 
