e083f7cf1caacc456f72812c735b84f95200cfbd
TallyJack <lynchj@infinity-software.com>
2026-05-26 16:53:18 -0400

    Completed Step 82

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -4,9 +4,14 @@ var A = "Website Reset";
 
 //This is where my functions are defined
 
-function crunchNums() { //This function returns the solution to the math question as a string
-    var V1 = 3 + 3;
-    document.getElementById("Math").innerHTML = "3 + 3 is " + V1;
+function addNums() { //This function returns the solution to the addition question as a string
+    var addition = 3 + 3;
+    document.getElementById("Math1").innerHTML = "3 + 3 is " + addition;
+}
+
+function subNums() { //This function returns the solution to the subtraction question as a string
+    var subtraction = 12 - 8;
+    document.getElementById("Math2").innerHTML = "12 - 8 is " + subtraction;
 }
 
 //This is the script where my statements are executed
