6cc7493c5dd8fe23db673c783f66381525b99e1e
TallyJack <lynchj@infinity-software.com>
2026-05-26 15:58:37 -0400

    Updated names of funcitons

--- Basic JavaScript Projects/Project2_Funcitons/JS/main.js
+++ Basic JavaScript Projects/Project2_Funcitons/JS/main.js
@@ -4,14 +4,14 @@ var A = "Website Reset";
 
 //This is where my functions are defined
 
-function Fun_1() { //This funciton changes the string values on both the text within the button and the text within the paragraph text that will display beneath it
+function Display_Text_on_Push() { //This funciton changes the string values on both the text within the button and the text within the paragraph text that will display beneath it
     var V1 = "I've Been Written!";
     var V2 = "I've Been Clicked!";
     document.getElementById("method_text").innerHTML = V1;
     document.getElementById("Button_Text").innerHTML = V2;
 }
 
-function Fun_2() { //This function adds more clauses to the existing paragraph text on the page
+function Concatenate_Strick_on_Push() { //This function adds more clauses to the existing paragraph text on the page
     var N1 = ", and here's another"
     document.getElementById("concat").innerHTML+=N1 
 }
