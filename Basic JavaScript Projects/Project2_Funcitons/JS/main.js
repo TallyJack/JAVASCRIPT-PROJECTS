bab6a5eeea4ab0f243c79e2c69d7755b6329e5a2
TallyJack <lynchj@infinity-software.com>
2026-05-26 15:05:50 -0400

    Completed Step 67

---
+++ Basic JavaScript Projects/Project2_Funcitons/JS/main.js
@@ -0,0 +1,16 @@
+//These are the definitions of my VARs
+var E = "<br>";
+var A = "Website Reset";
+
+//This is where my functions are defined
+function Fun_1() {
+    var V1 = "I've Been Written!";
+    var V2 = "I've Been Clicked!";
+    document.getElementById("method_text").innerHTML = V1;
+    document.getElementById("Button_Text").innerHTML = V2;
+}
+
+
+
+//This is the script where my statements are executed
+window.alert(A); //This simply helps indicate that the website has been refreshed
