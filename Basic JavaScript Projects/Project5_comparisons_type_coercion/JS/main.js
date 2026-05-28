86f1de6531f85b497247b1fb081362ffcaa37fb0
TallyJack <lynchj@infinity-software.com>
2026-05-28 19:05:52 -0400

    Completed Step 110

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -2,25 +2,26 @@
 var E = "<br>";
 var A = "Website Reset";
 
-//Script written for this project
-document.write(typeof "a");
-
-
-//These are the functions that I'm creating for this project
-
-
-//LEGACY FUNCTIONS
-function hero_traits() {
-    var Invincible = {//This is a dictionary containing various traits of a fictional superhero
-        Gender: "Male",
-        realName: "Mark Grayson",
-        Power: "Strength and Flight",
-        Age: 19,
-        Colors: "blue and yellow"
-    };
-    delete Invincible.realName;//This deletes the trait that will be called upon in the following line
-    document.getElementById("Dictionary").innerHTML = Invincible.realName;//This calls the deleted trait
-}
+//These are variables I'm establishing for the NaN Challenges
+var nanSub1 = 42;
+var nanSub2 = "The meaning of life...";
 
 //This is the script where my statements are executed
 window.alert(A); 
+document.getElementById("nanTest1").innerHTML = "1. True or False? The following datatype is not a number: " + nanSub1;
+document.getElementById("nanTest2").innerHTML = "2. True or False? The following datatype is not a number: " + nanSub2;
+
+//These are the functions that I'm creating for this project
+function dtReveal(x) {//This function replaces an element with it's data type
+    var DT = typeof document.getElementById(x).innerHTML;
+    document.getElementById(x).innerHTML = DT;
+}
+
+function sqrtReveal(x) {//This function replaces an element with its squareroot
+    var sqrtElement = Math.sqrt(document.getElementById(x).innerHTML);
+    document.getElementById(x).innerHTML = sqrtElement;
+}
+
+function nanReveal(x, y) {
+    document.getElementById(y).innerHTML = isNaN(x);
+}
