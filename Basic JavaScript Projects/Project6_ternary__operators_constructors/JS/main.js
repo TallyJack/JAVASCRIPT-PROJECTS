88eb58e3b720922f535f259bf09d164eb2946014
TallyJack <lynchj@infinity-software.com>
2026-06-02 16:10:36 -0400

    Completed Step 130

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -10,6 +10,13 @@ function Ride_Function() {
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
 
+function Vote_Function() {
+    var Age, Can_vote;
+    Age = document.getElementById("Age").value;
+    Can_vote = (Age < 18) ? "You are not old enough to" : "You can";
+    document.getElementById("Vote").innerHTML = Can_vote + " vote.";
+}
+
 
 //This is the script where my statements are executed
 window.alert(A); 
