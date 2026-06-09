df4d36cefb9e7191eea781bc214695d3b837f703
TallyJack <lynchj@infinity-software.com>
2026-06-09 14:58:52 -0400

    Corrected conditional statements according to feedback

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -87,10 +87,10 @@ function vowel_count() {
 function Time_function() {
     var Time = new Date().getHours();
     var Reply;
-    if (Time < 12 == Time > 0) {
+    if (Time < 12 && Time > 0) {
         Reply = "It is morning time!";
     }
-    else if (Time >= 12 == Time < 18) {
+    else if (Time >= 12 && Time < 18) {
         Reply = "It is afternoon.";
     }
     else {
