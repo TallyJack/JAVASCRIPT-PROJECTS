4a465dda94c3701a2820e359de9fe14dea6c2501
TallyJack <lynchj@infinity-software.com>
2026-06-12 10:48:34 -0400

    Completed Step 172

---
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,57 @@
+//These are definitions for some VARs that I find useful for test projects
+var E = "<br>";
+var A = "Website Reset";
+
+//The following assignment establishes the index for the shown slide
+let slideIndex = 1;
+showSlides(slideIndex);
+
+//These are the functions that I'm creating for this project
+
+//The following function enables the timer to countdown
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("Time's up!");
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+    tick();
+}
+
+//The following functions establish the behavior of the slideshow controls
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+//This is the script where my statements are executed
+window.alert(A); 
