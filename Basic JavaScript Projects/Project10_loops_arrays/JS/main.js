0ab686e39730c4f7dac312b71df5752f860fcd37
TallyJack <lynchj@infinity-software.com>
2026-06-16 17:40:05 -0400

    Completed Step 204

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -4,6 +4,10 @@ var A = "Website Reset";
 
 //These are the VARS that I'm creating for this project
 var coinRuns = 0; //This represents the number of times that the coin flip function has run.
+var head_Count = 0;
+var tail_Count = 0;
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var I;
 
 //These are the functions that I'm creating for this project
 
@@ -12,18 +16,29 @@ function Call_Loop() {
     var N = "";
     var X = 1;
     coinRuns++;
-    var Y = "<strong>Attempts: </strong>" + coinRuns;
     var isHeads = Math.random() >= 0.5;
     while (isHeads) {
+        head_Count++;
         N += E + X + ". Heads!";
         X++;
         isHeads = Math.random() >= 0.5;
     }
+    tail_Count++;
     N += E + X + ". Tails!";
+    var Y = "<strong>Attempts: " + coinRuns + E + "Head Count: " + head_Count + E + "Tails Count: " + tail_Count + E + "Heads/Tails Ratio: " + (head_Count/tail_Count).toPrecision(3) + "</strong>";
     document.getElementById("Loop").innerHTML = Y + N;
 
 }
 
+//The following function uses a for loop...
+function for_Loop() {
+    var content1 = "";
+    for (I = 0; I < Instruments.length; I++) {
+        
+        content1 += (I + 1) + ". " + Instruments[I] + E;
+    }
+    document.getElementById("List_of_Instruments").innerHTML = content1;
+}
 
 //This is the script where my statements are executed
 window.alert(A); 
