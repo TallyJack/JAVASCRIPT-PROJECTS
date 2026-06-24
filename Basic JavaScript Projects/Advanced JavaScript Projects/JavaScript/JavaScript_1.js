0c6f26ee0f622649cb6aa1df8932f6fe744b37d3
TallyJack <lynchj@infinity-software.com>
2026-06-24 17:45:42 -0400

    Completed Step 228

--- Basic JavaScript Projects/Advanced JavaScript Projects/JavaScript/JavaScript_1.js
+++ Basic JavaScript Projects/Advanced JavaScript Projects/JavaScript/JavaScript_1.js
@@ -5,7 +5,35 @@ var A = "Website Reset";
 //These are the VARS that I'm creating for this project
 
 //These are the functions that I'm creating for this project
+function Hero_Function() {
+    var Hero_Output;
+    var Hero = document.getElementById("Hero_Input").value;
+    var Hero_String = " is your favorite Hero!";
+    switch(Hero) {
+        case "Superman":
+            Hero_Output = "Superman" + Hero_String;
+        break;
+        case "Superman":
+            Hero_Output = "Batman" + Hero_String;
+        break;
+        case "Superman":
+            Hero_Output = "Aquaman" + Hero_String;
+        break;
+        case "Wonder Woman":
+            Hero_Output = "wonder Woman" + Hero_String;
+        break;
+        case "Cyborg":
+            Hero_Output = "Cyborg" + Hero_String;
+        break;
+        case "Flash":
+            Hero_Output = "Flash" + Hero_String;
+        break;
+        default:
+            Hero_Output = "Please enter the name of one the heroes exactly as written in the list above.";
 
+    }
+    document.getElementById("Hero_Output").innerHTML = Hero_Output;
+}
 
 //This is the script where my statements are executed
 window.alert(A); 
