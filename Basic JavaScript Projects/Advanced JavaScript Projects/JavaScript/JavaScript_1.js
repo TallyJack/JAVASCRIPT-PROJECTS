72ec40914ae63b07bc96a6ff7d5b7623df040d94
TallyJack <lynchj@infinity-software.com>
2026-06-24 17:30:01 -0400

    Established HTML_1 Assignment files

---
+++ Basic JavaScript Projects/Advanced JavaScript Projects/JavaScript/JavaScript_1.js
@@ -0,0 +1,11 @@
+//These are definitions for some VARs that I find useful for test projects
+var E = "<br>";
+var A = "Website Reset";
+
+//These are the VARS that I'm creating for this project
+
+//These are the functions that I'm creating for this project
+
+
+//This is the script where my statements are executed
+window.alert(A); 
