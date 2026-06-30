e3c6e56251b41aafa173396facefed4e1d37c82f
TallyJack <lynchj@infinity-software.com>
2026-06-30 17:36:18 -0400

    Completed Step 267

---
+++ Basic JavaScript Projects/Advanced JavaScript Projects/JavaScript/JavaScript_3.js
@@ -0,0 +1,15 @@
+//These are definitions for some VARs that I find useful for test projects
+var E = "<br>";
+var A = "Website Reset";
+
+//These are the variables that I'm creating for this project
+
+
+//These are the functions that I'm creating for this project
+function displayType(city) {
+    var cityState = city.getAttribute("data-state");
+    alert(city.innerHTML + " is a city in the state of " + cityState + "!");
+}
+
+//This is the script where my statements are executed
+window.alert(A); 
