258ea804a027237c1e7fd0066aed9b307a69ce63
TallyJack <lynchj@infinity-software.com>
2026-07-14 20:34:26 -0400

    Updated Step 309

--- Simple-Recipes/javascript/javascript.js
+++ Simple-Recipes/javascript/javascript.js
@@ -9,23 +9,29 @@ var modals = document.querySelectorAll(".recipe-modal");
 //Get the <span> element that closes the modal
 var closeBtn = document.getElementsByClassName("close-btn");
 
+//Closes the modal when called
+function modalClose() {
+    for (var index in modals) {
+            if (modals[index].style) {
+                modals[index].style.display = "none";
+            }
+        }
+}
+
 //When the user clicks the recipe button, open the modal
 for (var i = 0; i < btns.length; i++) {
     btns[i].onclick = function (event) {
         modal = document.querySelector(event.target.getAttribute("href"));
         modal.style.display = "block";
+        setTimeout(() => {modalClose();}, 10000);
     }
 }
 
 //When the user clicks on <span> (x), close the modal
 for (var i = 0; i < closeBtn.length; i++) {
     closeBtn[i].onclick = function () {
-        for (var index in modals) {
-            if (modals[index].style) {
-                modals[index].style.display = "none";
-            }
-        }
-    }
+        modalClose()
+    };
 }
 
 //EMAIL VALIDATION
